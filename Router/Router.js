c2ba5fb2bf53fc5f95c3887ac2c46696a0acb013
Islamhabibi <islem.habibi93@gmail.com>
2023-11-17 12:57:51 +0100

    v2

--- Router/Router.js
+++ Router/Router.js
@@ -1,11 +1,11 @@
 var express = require("express")
-const { sendreq, resive } = require("../Controllers/Controller")
+const { getProduct, AddProduct, DeletProduct, UpdateProduct, findProduct, deletProducts } = require("../Controllers/Controller")
 var router = express.Router()
-router.get("/get/:x/:y",sendreq)
-router.post("/post",resive)
-
-
-
-
+router.get("/get/",getProduct)
+router.post("/post/",AddProduct)
+router.delete("/delete/:id",DeletProduct)
+router.put("/update/:id",UpdateProduct)
+router.get("/get/:id",findProduct)
+router.delete("/delete/",deletProducts)
 module.exports = router
 
--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const express=require("express");
 const router = require("./Router/Router");
+const connection = require("./DB/db");
 const port = 5667
 const app= express() 
 app.use(express.json())
+connection()
 app.listen(port,console.log(`server is runing at http://192.168.3.63:${port}`))
-app.use("/",router)
+app.use("/product",router)
+
 
